aebe8d54a4d94d0d3ec29cceb28c52dc8a48bdf8
Milos Gregor <milos.gregor@gmail.com>
2015-10-08 10:13:24 +0200

    readme changed

--- sm-prototype-core/src/test/java/sm/ApplicationSecurityTest.java
+++ sm-prototype-core/src/test/java/sm/ApplicationSecurityTest.java
@@ -14,8 +14,8 @@ import org.springframework.test.web.servlet.request.RequestPostProcessor;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
