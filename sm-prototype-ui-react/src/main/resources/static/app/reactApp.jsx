38ce29d66519d1eccdaff67e4ada6ebe3a831fe2
mplewka <plewka.michal@gmail.com>
2015-10-09 12:33:43 +0200

    dummy table generated with React

--- sm-prototype-ui-react/src/main/resources/static/app/reactApp.jsx
+++ sm-prototype-ui-react/src/main/resources/static/app/reactApp.jsx
@@ -1,10 +1,40 @@
 var UserGrid = React.createClass({
     render: function() {
         return (
-            <div className="commentBox">
-                Hello, world! I am a CommentBox.
-            </div>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>ID2</th>
+                        <th>ID3</th>
+                        <th>Name</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>id</td>
+                        <td>id2</td>
+                        <td>id3</td>
+                        <td>name</td>
+                        <td className="btn-group-sm">
+                            <button className="btn btn-success" type="button" title="Edit">
+                                <span className="glyphicon glyphicon-edit"></span>
+                            </button>
+                            <button className="btn btn-danger" type="button" title="Delete">
+                                <span className="glyphicon glyphicon-trash"></span>
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         );
+    }
+});
+
+var UserRow = React.createClass({
+    render: function() {
+
     }
 });
 
